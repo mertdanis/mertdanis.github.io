6f11e807f4a525307eda7070e43a31443d50d7c1
M. D <mertdanis@outlook.com>
2022-12-26 14:01:59 +0300

    update

--- fylo-landing-page/app.js
+++ fylo-landing-page/app.js
@@ -1,23 +1,22 @@
-// let email = document.querySelector(`.section5__form`);
-// let emailBtn = document.querySelector(`.section5__submit`);
+let email = document.querySelector(`.section5__form`);
+let emailBtn = document.querySelector(`.section5__submit`);
 
-// let errorP = document.querySelector(`.section5__error`);
+let errorP = document.querySelector(`.section5__error`);
 
-// let checkvalidEmail = function () {
-//   if (!email.value.includes(`@`) && email.value != ``) {
-//     errorP.classList.add(`section5__error-active`);
-//     errorP.textContent = `Please enter a valid email address`;
-//     console.log(`not valid`);
-//   } else if (
-//     (email.value.includes(`@`) &&
-//       email.value[0] != `@` &&
-//       email.value.includes(`gmail.com`)) ||
-//     email.value.includes(`hotmail.com`) ||
-//     email.value.includes(`outlook.com`)
-//   )
-//     errorP.textContent = `valid`;
-//   errorP.classList.add(`valid`);
-//   console.log(`valid`);
-// };
+let checkvalidEmail = function () {
+  if (!email.value.includes(`@`) && email.value != ``) {
+    errorP.classList.add(`section5__error-active`);
+    errorP.textContent = `Please enter a valid email address`;
+    console.log(`not valid`);
+  } else if (
+    (email.value.includes(`@`) &&
+      email.value[0] != `@` &&
+      email.value.includes(`gmail.com`)) ||
+    email.value.includes(`hotmail.com`) ||
+    email.value.includes(`outlook.com`)
+  )
+    errorP.classList.remove(`section5__error-active`);
+  console.log(email.value);
+};
 
-// email.addEventListener(`click`, setInterval(checkvalidEmail, 1000));
+emailBtn.addEventListener(`click`, checkvalidEmail);
