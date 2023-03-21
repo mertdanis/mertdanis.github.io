532d8b6d661f7f666cbabc99170a6ba86bea6fe1
M. D <mertdanis@outlook.com>
2023-03-21 12:15:53 +0300

    update

--- multi-step-form/app.js
+++ multi-step-form/app.js
@@ -6,18 +6,22 @@ let step1Btn = document.querySelector(`#step1Btn`);
 let currentStep = 1;
 let stepSection = document.querySelector(`.section__left--step---number`);
 let steps = document.querySelector(`.step_${currentStep}`);
-let checkBox = document.querySelector(`#checkbox`);
+
+let fullName = document.querySelector(`#form__name`);
+let email = document.querySelector(`#form__email`);
+let tel = document.querySelector(`#form__tel`);
+
 ///////////////////////////////////////////////////////////////
 /// contents
 
-let step_2_Content = ` <div class="step_2">
+let step_2_Content = `   <div class="step_2">
 <h3 class="section__right--title">Select your plan</h3>
 <p class="section__right--desc">
   You have the option of monthly or yearly billing.
 </p>
 
 <div class="section__right--options">
-  <div class="section__right--option" id="option1">
+  <div class="section__right--option">
     <img
       src="/multi-step-form/imgs/assets/images/icon-arcade.svg"
       alt=""
@@ -26,6 +30,7 @@ let step_2_Content = ` <div class="step_2">
     <div class="section__right--option--content">
       <p class="section__right--option-title">Arcade</p>
       <p class="section__right--option-price">$9/mo</p>
+      <p class="section__right--option-discount">2 months free</p>
     </div>
   </div>
   <div class="section__right--option option2">
@@ -37,6 +42,7 @@ let step_2_Content = ` <div class="step_2">
     <div class="section__right--option--content">
       <p class="section__right--option-title">Advanced</p>
       <p class="section__right--option-price">$12/mo</p>
+      <p class="section__right--option-discount">2 months free</p>
     </div>
   </div>
   <div class="section__right--option option3">
@@ -48,6 +54,7 @@ let step_2_Content = ` <div class="step_2">
     <div class="section__right--option--content">
       <p class="section__right--option-title">Pro</p>
       <p class="section__right--option-price">$15/mo</p>
+      <p class="section__right--option-discount">2 months free</p>
     </div>
   </div>
 </div>
@@ -55,12 +62,12 @@ let step_2_Content = ` <div class="step_2">
   <p id="monthly" class="section__right--activated">Monthly</p>
   <label class="switch">
     <input type="checkbox">
-    <span class="slider round" id="checkbox"></span>
+    <span class="slider round" id="checkBox"></span>
   </label>
   <p id="yearly">Yearly</p>
 </div>
 
-</div
+</div>
 
 `;
 
@@ -157,79 +164,164 @@ let step_4_Content = `<div class="step_4">
 
 `;
 
+let step_5_Content = `    <div class="step5">
+<img src="/multi-step-form/imgs/assets/images/icon-thank-you.svg" alt="" class="step5__img">
+<h3 class="step5__title">Thank you!</h3>
+<p class="step5__desc">
+  Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
+</p>
+</div>`;
+
 ///////////////////////////////////////////////////////////////
 /// functions
 
-let btnsHandler = function () {
-  nextBtn.addEventListener(`click`, () => {
-    if (currentStep == 1) {
-      currentStep++;
+let inputChecker = function () {
+  let emails = [`outlook.com`, `gmail.com`, `hotmail.com`];
 
-      let deactiveStep = document.querySelector(
-        `#section__left-number--${currentStep - 1}`
-      );
-
-      deactiveStep.classList.remove(`section__left--step---number_active`);
-
-      let activeStep = document.querySelector(
-        `#section__left-number--${currentStep}`
-      );
-
-      activeStep.classList.add(`section__left--step---number_active`);
-
-      sectionRight.insertAdjacentHTML("afterbegin", step_2_Content);
-      document
-        .querySelector(`.step_${currentStep - 1}`)
-        .classList.add(`hidden`);
-
-      let options = document.querySelectorAll(`.section__right--option`);
-
-      options.addEventListener(`click`, (e) => {
-        console.log(e.target);
-      });
-    } else if (currentStep == 2) {
-      currentStep++;
-
-      deactiveStep = document.querySelector(
-        `#section__left-number--${currentStep - 1}`
-      );
-      deactiveStep.classList.remove(`section__left--step---number_active`);
-      activeStep = document.querySelector(
-        `#section__left-number--${currentStep}`
-      );
-      activeStep.classList.add(`section__left--step---number_active`);
-
-      sectionRight.insertAdjacentHTML("afterbegin", step_3_Content);
-      document
-        .querySelector(`.step_${currentStep - 1}`)
-        .classList.add(`hidden`);
-    } else if (currentStep == 3) {
-      currentStep++;
-      document.querySelector(`.step_${currentStep}`);
-      deactiveStep = document.querySelector(
-        `#section__left-number--${currentStep - 1}`
-      );
-
-      deactiveStep.classList.remove(`section__left--step---number_active`);
-
-      activeStep = document.querySelector(
-        `#section__left-number--${currentStep}`
-      );
-
-      activeStep.classList.add(`section__left--step---number_active`);
-
-      sectionRight.insertAdjacentHTML("afterbegin", step_4_Content);
-      document
-        .querySelector(`.step_${currentStep - 1}`)
-        .classList.add(`hidden`);
+  fullName.addEventListener(`mouseleave`, () => {
+    if (fullName.value.length > 3) {
+      fullName.classList.add(`section__right--form---input--valid`);
+    } else {
+      fullName.classList.remove(`section__right--form---input--valid`);
+      fullName.classList.add(`section__right--form---input--unvalid`);
     }
   });
+
+  email.addEventListener(`mouseleave`, () => {
+    if (email.value.includes(`@`) && email.value[0] != `@`) {
+      email.classList.add(`section__right--form---input--valid`);
+    } else {
+      email.classList.remove(`section__right--form---input--valid`);
+      email.classList.add(`section__right--form---input--unvalid`);
+    }
+  });
+
+  tel.addEventListener(`mouseleave`, () => {
+    if (tel.value.length == 10) {
+      tel.classList.add(`section__right--form---input--valid`);
+    } else {
+      tel.classList.remove(`section__right--form---input--valid`);
+      tel.classList.add(`section__right--form---input--unvalid`);
+    }
+  });
+};
+
+let btnsHandler = function () {
+  {
+    nextBtn.addEventListener(`click`, () => {
+      if (currentStep == 1) {
+        currentStep++;
+
+        let deactiveStep = document.querySelector(
+          `#section__left-number--${currentStep - 1}`
+        );
+
+        deactiveStep.classList.remove(`section__left--step---number_active`);
+
+        let activeStep = document.querySelector(
+          `#section__left-number--${currentStep}`
+        );
+
+        activeStep.classList.add(`section__left--step---number_active`);
+
+        sectionRight.insertAdjacentHTML("afterbegin", step_2_Content);
+        document
+          .querySelector(`.step_${currentStep - 1}`)
+          .classList.add(`hidden`);
+
+        let checkBox = document.querySelector(`#checkBox`);
+        let discount = document.querySelector(
+          `.section__right--option-discount`
+        );
+        checkBox.addEventListener(`click`, () => {
+          let step2Prices = document.querySelectorAll(
+            `.section__right--option-price`
+          );
+
+          step2Prices.forEach(function (price) {
+            let priceText = price.textContent;
+            let priceCalc = priceText.replace(/\D/g, "");
+            console.log(priceCalc);
+
+            let yearlyPrice = priceCalc * 10;
+            let monthlyPrice = priceCalc / 10;
+
+            console.log(yearlyPrice);
+            console.log(monthlyPrice);
+
+            if (monthly.classList.contains(`section__right--activated`)) {
+              yearly.classList.add(`section__right--activated`);
+              monthly.classList.remove(`section__right--activated`);
+              price.textContent = `$${yearlyPrice}/yr`;
+            } else {
+              monthly.classList.add(`section__right--activated`);
+              yearly.classList.remove(`section__right--activated`);
+              price.textContent = `$${monthlyPrice}/mo`;
+            }
+          });
+        });
+
+        let monthly = document.querySelector(`#monthly`);
+        let yearly = document.querySelector(`#yearly`);
+
+        let options = document.querySelectorAll(`.section__right--option`);
+
+        options.forEach(function (currentVal) {
+          currentVal.addEventListener(`click`, () => {
+            currentVal.classList.toggle(`section__right--option--check`);
+          });
+        });
+      } else if (currentStep == 2) {
+        currentStep++;
+
+        deactiveStep = document.querySelector(
+          `#section__left-number--${currentStep - 1}`
+        );
+        deactiveStep.classList.remove(`section__left--step---number_active`);
+        activeStep = document.querySelector(
+          `#section__left-number--${currentStep}`
+        );
+        activeStep.classList.add(`section__left--step---number_active`);
+
+        sectionRight.insertAdjacentHTML("afterbegin", step_3_Content);
+        document
+          .querySelector(`.step_${currentStep - 1}`)
+          .classList.add(`hidden`);
+      } else if (currentStep == 3) {
+        currentStep++;
+        document.querySelector(`.step_${currentStep}`);
+        deactiveStep = document.querySelector(
+          `#section__left-number--${currentStep - 1}`
+        );
+
+        deactiveStep.classList.remove(`section__left--step---number_active`);
+
+        activeStep = document.querySelector(
+          `#section__left-number--${currentStep}`
+        );
+
+        activeStep.classList.add(`section__left--step---number_active`);
+
+        sectionRight.insertAdjacentHTML("afterbegin", step_4_Content);
+        document
+          .querySelector(`.step_${currentStep - 1}`)
+          .classList.add(`hidden`);
+      } else if (currentStep == 4) {
+        currentStep++;
+        sectionRight.insertAdjacentHTML("afterbegin", step_5_Content);
+        document
+          .querySelector(`.step_${currentStep - 1}`)
+          .classList.add(`hidden`);
+        sectionRight.classList.add(`section__right--step5`);
+        backBtn.style.display = `none`;
+        nextBtn.style.display = `none`;
+      }
+    });
+  }
 
   backBtn.addEventListener(`click`, () => {
     if (currentStep > 1) {
       currentStep--;
-
-      console.log(currentStep);
 
       let deactiveStep = document.querySelector(
         `#section__left-number--${currentStep + 1}`
@@ -251,3 +343,4 @@ let btnsHandler = function () {
 };
 
 btnsHandler();
+setInterval(inputChecker, 1000);
